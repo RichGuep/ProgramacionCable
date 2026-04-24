9482d2d954828e53533b266f18e192fdf7a27d76
RichGuep <richard.guevara@greenmovil.com.co>
2026-04-24 12:16:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import streamlit as st
 from styles import apply_custom_styles
-from main_content import run_app # Aquí es donde estará tu menú
+from main_content import run_app
 
-# 1. Aplicamos el diseño
+# 1. Aplicamos configuración visual y CSS
 apply_custom_styles()
 
-# 2. Corremos la aplicación
+# 2. Iniciamos la aplicación
 if __name__ == "__main__":
     run_app()
