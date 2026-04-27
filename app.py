34f8fc743eba08064fbcdea3b6959ee383421ebd
RichGuep <richard.guevara@greenmovil.com.co>
2026-04-27 12:03:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,7 @@ import streamlit as st
 from main_content import run_app
 from styles import apply_global_styles
 
-# Configuración de página (Debe ser la primera instrucción de Streamlit)
-st.set_page_config(
-    page_title="MovilGo Admin",
-    page_icon="🚌",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
-# Aplicar los estilos globales definidos en styles.py
+st.set_page_config(page_title="MovilGo Admin", layout="wide")
 apply_global_styles()
 
 if __name__ == "__main__":
