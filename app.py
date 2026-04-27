8c502a66e8312e1e0bfb7a290c5e6da59246a856
RichGuep <richard.guevara@greenmovil.com.co>
2026-04-27 15:37:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from main_content import run_app
-from styles import apply_global_styles
 
 st.set_page_config(page_title="MovilGo Admin", layout="wide")
-apply_global_styles()
 
 if __name__ == "__main__":
     run_app()
